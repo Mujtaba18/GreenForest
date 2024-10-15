ba0342809dff9c7a030a639e357f8ea0f85f0151
H-aliwi <hussainjaffer84@gmail.com>
2024-10-15 13:44:35 -0700

    Delete park game done

--- src/components/ParkGames.jsx
+++ src/components/ParkGames.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+
 import axios from "axios"
 
 const ParkGames = () => {
   const { parkId } = useParams()
   const [games, setGames] = useState([])
   const [parkName, setParkName] = useState("")
+  const [message, setMessage] = useState("")
+  const [TotalTickets, setTotalTickets] = useState(0)
 
   useEffect(() => {
     const fetchParkGames = async () => {
@@ -15,8 +18,10 @@ const ParkGames = () => {
         )
         // get park name
         setParkName(response.data.park_name)
+
         console.log(response.data.games)
         setGames(response.data.games)
+        setTotalTickets(response.data.games.total_tickets)
       } catch (err) {
         console.error("Error:", err)
       }
@@ -25,13 +30,39 @@ const ParkGames = () => {
     fetchParkGames()
   }, [parkId])
 
+  // delete game
+
+  const deleteGame = async (gameId) => {
+    try {
+      await axios.delete(
+        `http://localhost:3001/parks/${parkId}/games/${gameId}`
+      )
+      setGames(games.filter((game) => game._id !== gameId))
+      setMessage("Game deleted successfully")
+      console.log("Game deleted successfully")
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  setTimeout(() => {
+    setMessage("")
+  }, 2000)
+
   return (
     <>
+      {message && <div className="message">{message}</div>}
       <h1>Games at Park {parkName}</h1>
       <div className="card-flex">
         {games && games.length > 0 ? (
           games.map((game) => (
             <div className="card" key={game._id}>
+              <button
+                onClick={() => deleteGame(game._id)}
+                style={{ float: "right", fontWeight: "700" }}
+              >
+                x
+              </button>
               <div className="img-wrapper">
                 <img
                   src={game.game_image}
@@ -43,6 +74,7 @@ const ParkGames = () => {
                 <h2>{game.game_name}</h2>
                 <p>Description: {game.game_description}</p>
                 <p>Price: ${game.game_price}</p>
+                <p>Total Tikets: {game.total_tickets}</p>
               </div>
             </div>
           ))
